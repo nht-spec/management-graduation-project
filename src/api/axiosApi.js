8db727e667b52de76a0857b007514880a6fafbf1
Nguyen Hoang Thinh <thinhnh25@fpt.com.vn>
2022-05-18 00:43:26 +0700

    change base url

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_KEYS } from '../constants/api-key';
 
 const axiosApi = axios.create({
-	baseURL: API_KEYS.BASE_URL_LOCAL,
+	baseURL: API_KEYS.BASE_URL,
 	headers: {
 		'Content-Type': 'application/json',
 		'Access-Control-Allow-Origin': '*',
